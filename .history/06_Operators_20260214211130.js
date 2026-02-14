469e266ef6485464c7d3551fe94da3ba28e2d375
hawamubarakk <hawamubarak007@gmail.com>
2026-02-14 21:24:35 +0500

    add file

---
+++ .history/06_Operators_20260214211130.js
@@ -0,0 +1,17 @@
+console.log("Operators in JS")
+
+let a = 45;
+let b = 4;
+
+console.log("a + b = ", a+b)
+console.log("a - b = ", a-b)
+console.log("a * b = ", a*b)
+console.log("a / b = ", a/b)
+console.log("a ** b = ", a**b) //a to the power b
+console.log("a % b = ", a%b) //remainder
+
+console.log("a++ : ",a++) //first a will be printed and then the increment is performed
+console.log("a = ",a)
+console.log("a-- : ",a--) //The same case as before
+
+
